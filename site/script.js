5b63f5d2f5d91a49e1ae33c2947811bf1471488a
chumm <chumpuridze249@gmail.com>
2021-07-09 17:41:15 +0300

    started practice

---
+++ site/script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+// ГОВНО СИНТАКСИС
+// function first() {
+//     var chbox=document.getElementById('one');
+// if (chbox.checked) {
+//     alert('Верно');
+// }
+// else {
+//     alert('ТЫ НЕПРАВ!!');
+// }
+// }
+
+// function second() {
+//     var radi=document.getElementsByName('r1')
+//     for (var i=0; i < radi.length; i++) {
+//         if (radi[i].checked) {
+//             alert('Выбран ' +i+ ' элемент')
+//         }
+//     }
+// }
+
+// А ВОТ ТУТ НОРМ
+// document.addEventListener('DOMContentLoaded', function() {
+//     var myCheckbox = document.querySelector('.my-checkbox');
+
+//     myCheckbox.addEventListener('change', function(){
+//         if (myCheckbox.checked){
+//             alert('Выбран');
+//         } else {
+//             alert('Не выбран');
+//         }
+//     })
+// }, false);
